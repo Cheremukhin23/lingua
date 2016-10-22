85afe9a10bb51914f63b9c606c500fc7afb60951
cheremukhin23 <dcheremukhin1@gmail.com>
2016-10-22 11:43:22 +0300

    create index file for actions

---
+++ imports/startup/client/actions/index.js
@@ -0,0 +1,21 @@
+import addList from './addList'
+import addWord from './addWord'
+import editListTitle from './editListTitle'
+import updateListTitle from './updateListTitle'
+import displayAddWordForm from './displayAddWordForm'
+import handleSingleWordDialog from './handleSingleWordDialog'
+import updateWord from './updateWord'
+import deleteList from './deleteList'
+import deleteWord from './deleteWord'
+
+export {
+  addList,
+  addWord,
+  editListTitle,
+  updateListTitle,
+  displayAddWordForm,
+  handleSingleWordDialog,
+  updateWord,
+  deleteList,
+  deleteWord
+}
